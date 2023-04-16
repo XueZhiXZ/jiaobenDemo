29ec73d91c882bf794f3418f5412ca0b5d854b48
dmq <2781725272@qq.com>
2023-04-16 20:09:03 +0800

    nodeltry

---
+++ youhoumoban.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://lenovo.ilive.cn/?f=stle
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ilive.cn
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
