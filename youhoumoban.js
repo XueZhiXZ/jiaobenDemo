9b85f7268034122bb95a4f76688178dd361eb7ea
dmq <2781725272@qq.com>
2023-04-18 21:15:14 +0800

    1

---
+++ youhoumoban.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       dmq
+// @match        https://lenovo.ilive.cn/?f=stle
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ilive.cn
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("Hello World");
+    console.log("111");
+})();
